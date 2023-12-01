47c9a8cc4b4418b0d5f8e2f3db9d8085d2948ec4
Seo Jin Kyu <seojk413@naver.com>
2023-12-01 14:46:10 +0900

    Player Sprite Changing by Moving Direction

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,17 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    public Sprite idleSprite; // Sprite when not moving
+    public Sprite upSprite;    // Sprite when moving up
+    public Sprite downSprite;  // Sprite when moving down
+    public Sprite leftSprite;  // Sprite when moving left
+    public Sprite rightSprite; // Sprite when moving right
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -28,5 +35,28 @@ public class PlayerMovement : MonoBehaviour
 
         // Move the character
         rb.velocity = movement * moveSpeed;
+        SetSprite(movement);
+    }
+    
+    void SetSprite(Vector2 movement)
+    {
+        if (movement.magnitude > 0.1f)
+        {
+            if (Mathf.Abs(movement.x) > Mathf.Abs(movement.y))
+            {
+                // Moving horizontally
+                spriteRenderer.sprite = movement.x > 0 ? rightSprite : leftSprite;
+            }
+            else
+            {
+                // Moving vertically
+                spriteRenderer.sprite = movement.y > 0 ? upSprite : downSprite;
+            }
+        }
+        else
+        {
+            // Not moving, set the idle sprite
+            spriteRenderer.sprite = idleSprite;
+        }
     }
 }
